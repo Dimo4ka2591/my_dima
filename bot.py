bcfdd94caf1174ffeff21481fd1eb2925643687e
Dimo4ka2591 <91nikola1991@mail.ru>
2026-08-25 01:32:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,1 @@
 
-                last_seen TEXT
-            )
-        """)
-        await db.commit()
-
-async def load_user(user_id):
-    async with aiosqlite.connect(DB_PATH) as db:
-        async with db.execute("SELECT first_name, username, gender FROM users WHERE user_id=?", (user_id,)) as cur:
-            row = await cur.fetchone()
-            if row:
